2b4f266cf6dba6b45e1ccba4644e2017a2121647
Rodion Yavelov <rodion.yavelov@yandex.ru>
2022-11-19 17:37:06 +0200

    Initial commit

---
+++ urlChecker.java
@@ -0,0 +1,38 @@
+public class urlChecker {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "https://www.linkedin.com"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+
+        if (url.startsWith("http")){
+            return "http";
+        } else if
+        (url.startsWith("https")){
+            return "https";
+        } else {
+            return "неизвестный";
+        }
+    }
+
+    public static String checkDomain(String url) {
+
+        if(url.endsWith("com")){
+            return "com";
+        } else if (url.endsWith("net")){
+            return "net";
+        } else if (url.endsWith("org")){
+            return "org";
+        } else if (url.endsWith("ru")){
+            return "ru";
+        } else {
+            return "неизвестный";
+        }
+    }
+}
